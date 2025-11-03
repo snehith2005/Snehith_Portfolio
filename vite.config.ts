ef5285b4b11635dd154ba82a387d864fd3867bd8
SnehithReddy <nallapuramsnehithreddy@gmail.com>
2025-11-03 23:23:03 +0530

    edit vited

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,24 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // ensures correct asset paths in production
+
   server: {
-    host: "::",
-    port: 8080,
-    allowedHosts:["snehith-portfolio.onrender.com"]
+    host: true,  // instead of "::"
+    port: 8080,  // for local dev
   },
+
+  preview: {
+    host: true,  // required for Render
+    port: 10000, // Render uses this internally
+  },
+
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
